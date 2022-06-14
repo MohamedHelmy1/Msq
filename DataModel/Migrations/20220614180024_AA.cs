9e41bd096bd5c381b0fdcd477df2bd42570edf7b
mohamed121helmy@gmail.com <mohamed121helmy@gmail.com>
2022-06-14 20:49:50 +0200

    a

---
+++ DataModel/Migrations/20220614180024_AA.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Batabase.Migrations
+{
+    public partial class AA : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "cuntery",
+                table: "user",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "job",
+                table: "user",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "phone",
+                table: "user",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cuntery",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "job",
+                table: "user");
+
+            migrationBuilder.DropColumn(
+                name: "phone",
+                table: "user");
+        }
+    }
+}
